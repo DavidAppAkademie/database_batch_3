492971c11d44cfa4680370faed0d3859991a660b
David Sent <david.sent@app-akademie.com>
2024-01-30 11:58:36 +0100

    Remove unnecessary delays in MockRepository

--- lib/features/crud/data/mock/mock_repository.dart
+++ lib/features/crud/data/mock/mock_repository.dart
@@ -7,25 +7,21 @@ class MockRepository implements DatabaseRepository {
   MockRepository(this._mockService);
   @override
   Future<void> createParticipantList(List<Participant> participants) async {
-    await Future.delayed(const Duration(seconds: 2));
     _mockService.create();
   }
 
   @override
   Future<void> deleteParticipantList() async {
-    await Future.delayed(const Duration(seconds: 2));
     _mockService.deleteList();
   }
 
   @override
   Future<List<Participant>?> readParticipantList() async {
-    await Future.delayed(const Duration(seconds: 2));
     return _mockService.getAll();
   }
 
   @override
   Future<bool> updateParticipantList(List<Participant> participants) async {
-    await Future.delayed(const Duration(seconds: 2));
     _mockService.updateList(participants);
     return true;
   }
